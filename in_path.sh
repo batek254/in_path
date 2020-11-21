aa3396dd6a32d4c8468d84176849bf5c8aeb293b
Bartlomiej Fatyga <batek254@gmail.com>
2020-11-22 00:53:52 +0100

    searching file in given path

--- in_path.sh
+++ in_path.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 #script that verifies if given program is in PATH directory list
+
+cmd=$1
+ourpath=$2
+result=1
+oldIFS=$IFS
+IFS=":"
+
+for directory in "$ourpath"
+do
+    echo $directory
+    echo $ourpath
+    if [ -x $directory/$cmd ] ; then #-x FILE exists and the execute permission is granted.
+        result=0
+    fi
+done
+
+IFS=$oldIFS
+echo $result
